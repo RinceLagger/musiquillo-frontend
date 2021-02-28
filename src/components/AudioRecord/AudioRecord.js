db3d756ce0f9f1d9841584b4a35ab5962145ae2e
Pablo Fernandez <pablo_fz@hotmail.com>
2021-02-28 22:36:20 +0100

    AudioRecord component graba sonido y se puede reproducir aunque aun no se oye

---
+++ src/components/AudioRecord/AudioRecord.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+export default function AudioRecord () {
+
+    const [source, setSource] = React.useState(null);
+    const [sourcePlay, setSourcePlay] = React.useState(null);
+    //const [chunks, setChunks] = React.useState([]);
+    const [mediaRecorder, setmediaRecorder] = React.useState({});
+
+    let chunks = [];
+
+    mediaRecorder.onstart = function(e) {
+        chunks = [];
+    };
+
+    mediaRecorder.ondataavailable = function(e) {
+        console.log(e)
+        //console.log(mediaRecorder.requestData())
+        chunks.push(e.data);
+        //setChunks(chunks);
+        
+    };
+
+    mediaRecorder.onstop = function(e) {
+        //console.log(mediaRecorder.requestData())
+        let blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+        setSourcePlay(window.URL.createObjectURL(blob));
+        console.log(blob)
+    };
+
+    
+
+
+    React.useEffect(()=>{
+
+        let constraints = { audio: true };
+
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(mediaStreamObj) {
+           
+            setSource(mediaStreamObj);
+        
+            setmediaRecorder(new MediaRecorder(mediaStreamObj));
+            
+            })
+       
+
+    },[]);
+
+    const startRecord = () => {
+        try {
+            mediaRecorder.start();
+            console.log(mediaRecorder.state)
+        }catch(e){
+            console.error(e)
+        }
+        
+    }
+
+    const stopRecord = () => {
+        try{
+            //console.log(mediaRecorder.requestData())  
+        mediaRecorder.stop();
+        }catch(e){
+        console.error(e)
+    }
+    }
+
+    return(
+        <div>
+        {console.log(mediaRecorder)}
+            <button id="start-record" onClick = {startRecord}>Start Recording</button>
+            <button id="stop-record" onClick = {stopRecord}>Stop Recording</button>
+            {/* <button id="play" onClick = {playRecord}>Start Playing</button>
+            <button id="stop" onClick = {stopRecord}>Stop Playing</button> */}
+
+            {/* <audio src={source} id="record" className = "audio-controls" controls></audio> */}
+            <audio src={sourcePlay} id="play" className = "audio-controls" controls></audio>
+        </div>
+
+    )
+}
