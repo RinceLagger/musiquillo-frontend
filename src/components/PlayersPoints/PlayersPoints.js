2e59456ee419b7dc56b373d9fede873919f5a50a
Pablo Fernandez <pablo_fz@hotmail.com>
2021-03-21 20:14:25 +0100

    indicacion de cantante en lista jugadores

--- src/components/PlayersPoints/PlayersPoints.js
+++ src/components/PlayersPoints/PlayersPoints.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { usePlayers } from "../../context/PlayersContext";
 import "./PlayerPoints.css";
+import { useTurn } from "../../context/TurnContext";
 
-export default function PlayersPoints ({styleName:{playersStyle}}){
+export default function PlayersPoints({ styleName: { playersStyle } }) {
+  const { turn } = useTurn();
 
-    const { players} = usePlayers();
+  const { players } = usePlayers();
 
-
-    return(
-        <ul className = {playersStyle}>{players.map((player)=> <li key = {player._id}>
-        <span>{player.username} </span>
-        <span>Points: {player.points}</span></li>)}
-        </ul>
-    )
-
+  return (
+    <ul className={playersStyle}>
+      {players.map((player, index) => {
+        if (turn === index && playersStyle==="ingame-points") {
+          return (
+            <li key={player._id} id="turn">
+              <span>{player.username} </span>
+              <span>Points: {player.points}</span>
+            </li>
+          );
+        } else {
+          return (
+            <li key={player._id}>
+              <span>{player.username} </span>
+              <span>Points: {player.points}</span>
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
 }
